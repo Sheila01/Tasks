ff4ac9154d8c14142b7cfc40e59dd111c319bffa
Sheila Colorado <sheilacolorado1997@gmail.com>
2023-03-03 12:52:43 +0100

    I don't understand anything

--- Back/index.js
+++ Back/index.js
@@ -1,8 +1,6 @@
 import express from 'express';
-import  {PORT}  from '.config';
+import PORT from './config';
 const app = express();
 
-
-app.listen(PORT, () => {
-  console.log(`This is my ${PORT}`);
-});
+app.listen(PORT)
+console.log(`This is my ${PORT}`);
